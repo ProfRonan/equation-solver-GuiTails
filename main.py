ee539d39ac41045ef74330c63e72ddf71052000a
Guilherme <128387905+GuiTails@users.noreply.github.com>
2023-04-12 19:23:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ if N ==1:
         x = int
         x = -b / a
         a*x+b==0
-        print([N, a, b], ["A equação é do primeiro grau", x])
+        print(x)
+        print(a*x+b==0)
 
 if N ==2:
-    print("A equação é do segundo grau")
-    a = int(input("> "))
+    a = int(input("A equação é do segundo grau"))
     if a ==0:
         print("Valor de a inválido")
     if a !=0:
@@ -26,14 +26,16 @@ if N ==2:
         x = int
         delta = b**2 + (-4)*a*c
 
-        if delta >= 0:
-            x1 = (-b + (delta**0.5)) / (2*a)
-            x2 = (-b - (delta**0.5)) / (2*a) 
         if delta > 0:
+            x1 = (-b - (delta**0.5)) / (2*a) 
+            x2 = (-b + (delta**0.5)) / (2*a)
             print("A equação possui duas raízes reais")
             print(x1,x2)
         if delta ==0:
+            x3 = (-b + (delta**0.5)) / (2*a)
             print("A equação possui apenas uma raiz real")
-            print(x1,x2)
+            print(x3)
         if delta < 0:
+            x1 = (-b - (delta**0.5)) / (2*a) 
+            x2 = (-b + (delta**0.5)) / (2*a) 
             print("A equação não possui raízes reais")

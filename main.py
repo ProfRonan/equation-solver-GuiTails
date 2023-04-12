3d9001cf2b4fbbe5ef11e27213bba35065f85541
Guilherme <128387905+GuiTails@users.noreply.github.com>
2023-04-12 19:34:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -33,10 +33,8 @@ if N ==2:
             print("A equação possui duas raízes reais")
             print(x1,x2)
         if delta ==0:
-            x3 = (-b + (delta**0.5)) / (2*a)
+            x3 = (-b / (2*a))
             print("A equação possui apenas uma raiz real")
             print(x3)
         if delta < 0:
-            x1 = (-b - (delta**0.5)) / (2*a) 
-            x2 = (-b + (delta**0.5)) / (2*a) 
             print("A equação não possui raízes reais")
